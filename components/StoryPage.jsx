1b1614e5942726ce86e83040d3775f264f8b08c5
Gregory Bober <g.bober@gmail.com>
2021-05-11 13:46:28 +0800

    story page independent story loading

--- components/StoryPage.jsx
+++ components/StoryPage.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { useRecoilCallback, useRecoilState, useRecoilValueLoadable } from 'recoil'
+import { useSetRecoilState, useRecoilState } from 'recoil'
 
 import { db } from '../utils/firebase'
 import Comments from './Comments'
@@ -10,7 +10,7 @@ import UserText from './UserText'
 import { 
 	openedStorySelector, 
 	openStoryIdAtom, 
-	storyItemSelector 
+	storyAtom, 
 } from '../utils/atoms'
 
 
@@ -18,24 +18,39 @@ import {
 export default function StoryPage() {
 
 	const [storyId, setOpenStoryId] = useRecoilState(openStoryIdAtom)
-	const loadable = useRecoilValueLoadable(storyItemSelector(storyId))
-	const [openedStory, setOpenedStory] = useRecoilState(openedStorySelector(storyId))
+	const setOpenedStory = useSetRecoilState(openedStorySelector(storyId))
+	const [story, setStory] = useRecoilState(storyAtom(storyId))
 
 
-	const refresh = useRecoilCallback(({ set }) => async (id) => {
-		console.log('refetch story', storyId, id)
-		const snap = await db.child(`item/${storyId}`).get()
-		const val = snap.val()
-		console.log(val)
-		set(storyItemSelector(storyId), val || null)
-	})
+	// const refresh = useRecoilCallback(({ set }) => async (id) => {
+	// 	console.log('refetch story', storyId, id)
+	// 	const snap = await db.child(`item/${storyId}`).get()
+	// 	const val = snap.val()
+	// 	console.log(val)
+	// 	set(storyItemSelector(storyId), val || null)
+	// })
 
 	// React.useEffect(() => {
 	// 	const intervalId = setInterval(refresh, 2000)
 	// 	return () => clearInterval(intervalId)
 	// }, [refresh])
 
-	const story = loadable.state === 'hasValue' ? loadable.contents : null
+	// const story = loadable.state === 'hasValue' ? loadable.contents : null
+
+	React.useEffect(() => {
+		async function fetchStory() {
+			try {
+				console.log('fetchStory')
+				const snap = await db.child(`item/${storyId}`).get()
+				setStory(snap.val())
+			} catch (error) {
+				console.warn("couldn't fetch story", storyId, error)
+			}
+		}
+		if (story === null)
+			fetchStory()
+	}, [story])
+
 
 	React.useEffect(() => {
 		if (story) {
@@ -44,17 +59,18 @@ export default function StoryPage() {
 		}
 	}, [setOpenedStory, story])
 	
-	if (loadable.state === 'hasError') {
-		console.warn('story hasError', storyId)
-		return <li>error</li>
-	}
+	// if (loadable.state === 'hasError') {
+	// 	console.warn('story hasError', storyId)
+	// 	return <li>error</li>
+	// }
 
-	if (loadable.state === 'loading')
-		return <div id='StoryPage'>Loading...</div>
+	// if (loadable.state === 'loading')
+	// 	return <div id='StoryPage'>Loading...</div>
 
 	if (!story) { 
-		console.warn('no story', storyId)
-		return null
+		return <div id='StoryPage'>
+			<p>Loading...</p>
+		</div>
 	}
 	
 	return <div id='StoryPage'>
